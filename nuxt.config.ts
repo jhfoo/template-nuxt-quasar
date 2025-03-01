45e3872cbe279ee52670179ef2498d80e72ae781
Foo JH <foojihaw@hotmail.com>
2025-03-01 11:22:15 +0700

    NEW: Quasar components on home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,22 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxt-quasar-ui']
+  modules: ['nuxt-quasar-ui'],
+  quasar: {
+    // Optional string | boolean
+    // sassVariables: "assets/quasar.variables.scss",
+    // Optional string[]
+    // If you use animations, add Quasar Extra CSS animation URL here.
+    // NOTE: This CSS files are inserted on module plugin template, NOT on `nuxt.config`.
+    // css: ["@quasar/extras/material-icons/material-icons.css"],
+    // List of extra Quasar Plugins
+    // auto-instaled: [Platform, Body, Dark, Screen, History, Lang, IconSet]
+    // optional: [AddressbarColor, AppFullscreen, AppVisibility, BottomSheet, Dialog,
+    //            LoadingBar, Loading, Notify, LocalStorage, SessionStorage]
+    // plugins: ["Dialog"],
+    /* Quasar UI config -- you'll notice in Quasar docs when you need it */
+    config: {
+      dark: true,
+    },
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="q-pl-lg">
+    <p class="text-h6 q-pt-md">
+      Plugin Showcase
+    </p>
+    <q-list>
+      <q-item
+        v-for="(button, idx) in buttons"
+        :key="idx"
+      >
+        <q-btn
+          v-bind="button"
+        />
+      </q-item>
+    </q-list>
+    <p class="text-h6 q-pt-md">
+      Directive Showcase
+    </p>
+    <example-list />
+    <p class="text-h6 q-pt-md">
+      Quasar Default Prop Values
+    </p>
+    <prop-defaults />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+</script>
+
